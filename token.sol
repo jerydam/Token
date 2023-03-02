7aa9d617de5f407e0b736ce0b7a84033b4cddd62
Jeremiah Oyeniran Damillare <jerydan148@gmail.com>
2023-03-02 20:49:00 +0100

    Update token.sol

--- token.sol
+++ token.sol
@@ -5,6 +5,6 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract D0peToken is ERC20 {
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(msg.sender, 20 * 20 ** 18);
+        _mint(msg.sender, 20 * 10 ** 18);
     }
 }
